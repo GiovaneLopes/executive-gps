2d1caa46696fc7e5202efb497e6bc3e8800cba43
Giovane Lopes <38773201+GiovaneLopes@users.noreply.github.com>
2025-09-13 20:15:49 -0300

    Fix app language

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:executive_gps/src/app_module.dart';
 import 'package:executive_gps/src/app_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 import 'package:executive_gps/firebase_options_dev.dart' as dev;
 import 'package:executive_gps/firebase_options_prod.dart' as prod;
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterLocalization.instance.ensureInitialized();
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
   );
--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 import 'package:executive_gps/modules/shared/resources/app_colors.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final localization = FlutterLocalization.instance;
+  @override
+  void initState() {
+    _configureLocalization();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +28,8 @@ class AppWidget extends StatelessWidget {
       builder: (_, child) {
         return MaterialApp.router(
           routerConfig: Modular.routerConfig,
+          supportedLocales: localization.supportedLocales,
+          localizationsDelegates: localization.localizationsDelegates,
           theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(
             seedColor: AppColors.primary,
@@ -22,5 +37,15 @@ class AppWidget extends StatelessWidget {
         );
       },
     );
+  }
+
+  void _configureLocalization() {
+    localization.init(
+      mapLocales: [
+        const MapLocale('pt', {'title': 'Executive GPS'}),
+      ],
+      initLanguageCode: 'pt',
+    );
+    localization.onTranslatedLanguage = (locale) => setState(() {});
   }
 }
